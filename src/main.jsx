8ff1af162ef46efe8218e12bf760fd30540ef862
Gamora00 <almodieljethro16@gmail.com>
2026-05-23 23:58:00 +0800

    revmove base

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import { BrowserRouter } from 'react-router-dom';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <BrowserRouter >
         <App />
       </BrowserRouter>
     </Provider>
